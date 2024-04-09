9c3a24e777592ea36b0c2d3f1b2bde12d2407cf7
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2024-04-10 03:55:11 +1000

    Improved instagram session get element code
    All get element code now goes through one function with appropriate error handling to log failed element references.

--- InstaBioBot/src/main/kotlin/au/jamal/instabiobot/instagram/InstagramSession.kt
+++ InstaBioBot/src/main/kotlin/au/jamal/instabiobot/instagram/InstagramSession.kt
@@ -3,7 +3,7 @@ package au.jamal.instabiobot.instagram
 import au.jamal.instabiobot.utilities.DelayControl
 import au.jamal.instabiobot.utilities.Log
 import org.openqa.selenium.By
-import org.openqa.selenium.TimeoutException
+import org.openqa.selenium.WebElement
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import java.time.Duration
@@ -11,55 +11,46 @@ import java.time.LocalDateTime
 
 const val INSTAGRAM_URL = "https://www.instagram.com/"
 const val INSTAGRAM_SETTINGS_URL = "https://www.instagram.com/accounts/edit/"
+const val SELENIUM_TIMEOUT = 10L
 
 class InstagramSession (production: Boolean, debug: Boolean) {
 
     private val session: BrowserManager = BrowserManager(production, debug)
-    private val sessionWait: WebDriverWait = WebDriverWait(session.browser, Duration.ofSeconds(10))
+    private val sessionWait: WebDriverWait = WebDriverWait(
+        session.browser, Duration.ofSeconds(SELENIUM_TIMEOUT)
+    )
 
     fun login() {
         val (username, password) = LoginSecrets.envLoad()
         session.browser.get(INSTAGRAM_URL)
         DelayControl.sleep(5, 10)
-        val usernameInput = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='username']"))
-        )
-        val passwordInput = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[name='password']"))
-        )
+        val usernameInput = getElement(By::cssSelector, "input[name='username']")
+        val passwordInput = getElement(By::cssSelector, "input[name='password']")
+        val loginButton = getElement(By::xpath, "//button[@type='submit']")
         usernameInput.sendKeys(username)
         passwordInput.sendKeys(password)
-        val loginButton = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@type='submit']"))
-        )
         loginButton.click()
         DelayControl.sleep(5, 10)
-        verifySettingsPage()
+        accessSettings() // Verifies login
         Log.status("Login successful at ${LocalDateTime.now()}")
     }
 
     fun getCurrentBio (): String {
-        verifySettingsPage()
-        val bioElement = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.cssSelector("textarea[id='pepBio']"))
-        )
+        accessSettings()
+        val bioElement = getElement(By::cssSelector, "textarea[id='pepBio']")
         val bioText = bioElement.getAttribute("value")
         Log.status("Current bio text: [$bioText] at ${LocalDateTime.now()}")
         return bioText
     }
 
     fun updateBio (newBioText: String) {
-        verifySettingsPage()
-        val bioElement = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.cssSelector("textarea[id='pepBio']"))
-        )
+        accessSettings()
+        val bioElement = getElement(By::cssSelector, "textarea[id='pepBio']")
+        val updateButton = getElement(By::xpath, "//*[contains(text(), 'submit')]")
         bioElement.clear()
         bioElement.sendKeys(newBioText)
-        val updateButton = sessionWait.until(
-            ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'submit')]"))
-        )
         updateButton.click()
-        // Verify bio update
+        // TODO: Verify bio update
         Log.status("Updated bio text: [$newBioText] at ${LocalDateTime.now()}")
     }
 
@@ -68,13 +59,25 @@ class InstagramSession (production: Boolean, debug: Boolean) {
         session.end()
     }
 
-    private fun verifySettingsPage() {
+    private fun getElement(selector: (String) -> By, expression: String): WebElement {
+        try {
+            val locator = selector(expression)
+            return sessionWait.until(
+                ExpectedConditions.presenceOfElementLocated(locator)
+            )
+        } catch (e: Exception) {
+            Log.alert("Failed to access element: [$selector, $expression]")
+            throw IllegalStateException("Invalid DOM references...")
+        }
+    }
+
+    private fun accessSettings() {
         if (session.browser.currentUrl != INSTAGRAM_SETTINGS_URL) {
             session.browser.get(INSTAGRAM_SETTINGS_URL)
             DelayControl.sleep(2, 5)
             if (session.browser.currentUrl != INSTAGRAM_SETTINGS_URL) {
-                Log.alert("Failed to access Instagram settings")
-                throw IllegalStateException("Verify session login...")
+                Log.alert("Failed to access settings")
+                throw IllegalStateException("Session login issue detected...")
             }
         }
     }
