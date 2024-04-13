04a094a58fd394ca249948bbf24db2db68a93150
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2024-04-14 00:23:27 +1000

    Updated todo comment

--- InstaBioBot/src/main/kotlin/au/jamal/instabiobot/Main.kt
+++ InstaBioBot/src/main/kotlin/au/jamal/instabiobot/Main.kt
@@ -4,4 +4,4 @@ import au.jamal.instabiobot.control.SessionController
 
 fun main() {
     SessionController.mainSessionLoop(production = false, debug = true)
-} // TODO: implement provision of arguments
+} // TODO: implement provision of arguments and shutdown signal
