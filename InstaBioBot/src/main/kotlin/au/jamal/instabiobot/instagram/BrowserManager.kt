f9eda91624f2832693d62e6df527e501dfcfb7c5
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2024-04-14 23:03:02 +1000

    Added error handling to browser session starting

--- InstaBioBot/src/main/kotlin/au/jamal/instabiobot/instagram/BrowserManager.kt
+++ InstaBioBot/src/main/kotlin/au/jamal/instabiobot/instagram/BrowserManager.kt
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait
 import java.net.URI
 import java.time.Duration
 import java.util.logging.Level
+import kotlin.system.exitProcess
 
 class BrowserManager(production: Boolean, debug: Boolean, timeout: Long) {
 
@@ -16,25 +17,9 @@ class BrowserManager(production: Boolean, debug: Boolean, timeout: Long) {
     val wait: WebDriverWait
 
     init {
-        val options = ChromeOptions()
         Log.info("Starting Selenium: production [$production], debug [$debug]")
-        if (!debug) {
-            options.addArguments("--headless")
-            options.addArguments("--disable-logging")
-            options.setExperimentalOption("excludeSwitches", listOf("enable-automation"))
-            java.util.logging.Logger.getLogger("org.openqa.selenium").level = Level.OFF
-        } else {
-            Log.warn("Running in debug mode...")
-        }
-        if (production) {
-            browser = RemoteWebDriver(
-                URI.create("http://selenium:4444/wd/hub").toURL(),
-                options
-            )
-        } else {
-            browser = ChromeDriver(options)
-            Log.warn("Running in local mode...")
-        }
+        val options = configureOptions(debug)
+        browser = startBrowser(options, production)
         browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(timeout))
         wait = WebDriverWait(browser, Duration.ofSeconds(timeout))
     }
@@ -48,6 +33,41 @@ class BrowserManager(production: Boolean, debug: Boolean, timeout: Long) {
         } catch (e: Exception) {
             Log.alert("Failed to kill selenium driver")
         }
+    }
+
+    private fun configureOptions(debug: Boolean): ChromeOptions {
+        val options = ChromeOptions()
+        if (!debug) {
+            options.addArguments("--headless")
+            options.addArguments("--disable-logging")
+            options.setExperimentalOption("excludeSwitches", listOf("enable-automation"))
+            java.util.logging.Logger.getLogger("org.openqa.selenium").level = Level.OFF
+        } else {
+            Log.warn("Running in debug mode...")
+        }
+        return options
+    }
+
+    private fun startBrowser(options: ChromeOptions, production: Boolean): WebDriver {
+        val browser: WebDriver = try {
+            if (production) {
+                val remoteDriver = RemoteWebDriver(
+                    URI.create("http://selenium:4444/wd/hub").toURL(),
+                    options
+                )
+                Log.status("Started production browser session")
+                remoteDriver
+            } else {
+                val localDriver = ChromeDriver(options)
+                Log.warn("Running in local mode...")
+                localDriver
+            }
+        } catch (e: Exception) {
+            Log.alert("Failed to start production browser...")
+            Log.error(e)
+            exitProcess(0)
+        }
+        return browser
     }
 
 }
