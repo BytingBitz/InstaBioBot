63a3c916bf00a4251c0af0b9d5707e70dc6517c4
Jamal135 <50566368+Jamal135@users.noreply.github.com>
2024-04-14 22:02:13 +1000

    Completed config loading and building

--- InstaBioBot/src/main/kotlin/au/jamal/instabiobot/Main.kt
+++ InstaBioBot/src/main/kotlin/au/jamal/instabiobot/Main.kt
@@ -4,6 +4,6 @@ import au.jamal.instabiobot.control.ConfigHandler
 import au.jamal.instabiobot.control.SessionController
 
 fun main() {
-    val config = ConfigHandler.loadConfig()
+    val config = ConfigHandler.loadSettings()
     SessionController.mainSessionLoop(production = false, debug = true)
 } // TODO: pass config into loop
--- InstaBioBot/src/main/kotlin/au/jamal/instabiobot/control/ConfigHandler.kt
+++ InstaBioBot/src/main/kotlin/au/jamal/instabiobot/control/ConfigHandler.kt
@@ -3,6 +3,7 @@ package au.jamal.instabiobot.control
 import au.jamal.instabiobot.utilities.Log
 import org.yaml.snakeyaml.Yaml
 import java.io.File
+import kotlin.system.exitProcess
 
 data class ConfigSettings(
     val production: Boolean = true,
@@ -15,28 +16,63 @@ object ConfigHandler {
     private const val CONFIG_PATH = "config.yml"
 
     private val configFile = File(CONFIG_PATH)
+    private val configClass = ConfigSettings::class.java
     private val yaml = Yaml()
 
-    fun loadConfig(): ConfigSettings {
-        Log.status("Loading config...")
+    fun loadSettings(): ConfigSettings {
         if (!configFile.exists()) {
             Log.warn("No config file exists, generated default:")
             buildConfig()
         }
-        val inputStream = configFile.inputStream()
-        val settings = inputStream.use {
-            val data = yaml.load(inputStream) as Map<String, Any>
-            val production = data["production"] as Boolean
-            val debug = data["debug"] as Boolean
-            val timeout = (data["timeout"] as Number).toLong()
-            ConfigSettings(production, debug, timeout)
-        } // TODO: Add error handling on value loading
+        val settings = getConfig()
+        Log.status("Loaded config:")
         Log.dump(settings)
         return settings
     }
 
     private fun buildConfig() {
-        // TODO: Implement effective build
+        val yamlString = buildString {
+            configClass.declaredFields.forEach { field ->
+                field.isAccessible = true
+                appendLine("${field.name}: ${field.get(ConfigSettings())}")
+            }
+        }
+        configFile.writeText(yamlString)
+    }
+
+    private fun getConfig(): ConfigSettings {
+        val inputStream = configFile.inputStream()
+        inputStream.use {
+            val data = yaml.load(inputStream) as Map<String, Any>
+            val args = configClass.declaredFields.map { field ->
+                val fieldName = field.name
+                val fieldValue = data[fieldName] ?: field.get(ConfigSettings())
+                fieldValue
+            }.toTypedArray()
+            return constructSettings(args, data)
+        }
+    }
+
+    private fun constructSettings(args: Array<Any>, data: Map<String, Any>): ConfigSettings {
+        try{
+            val constructor = configClass.constructors.find { it.parameterCount == data.size }
+                ?: throw IllegalArgumentException("No matching constructor found")
+            return constructor.newInstance(*args) as ConfigSettings
+        } catch (e: IllegalArgumentException) {
+            Log.alert("Failed to build config, verify expected types:")
+            val expectedTypes = getExpectedTypes()
+            Log.dump(expectedTypes)
+            Log.alert("Exiting session launch...")
+            exitProcess(0)
+        }
+    }
+
+    private fun getExpectedTypes(): String {
+        val configFields = ConfigSettings::class.java.declaredFields
+        val keyTypeArray = configFields.map { field ->
+            field.name to field.type.simpleName
+        }.toTypedArray()
+        return keyTypeArray.contentToString()
     }
 
 }
